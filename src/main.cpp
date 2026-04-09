4f4cad06e83d7924c0315b98f555f8230f6c0719
MaciejGit6 <macie.milewski@gmail.com>
2026-04-09 16:25:02 +0200

    I added Breakpoint class

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,37 @@
 #include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
+#include <cstdint> 
+
+class Breakpoint {
+public:
+    Breakpoint() = default;
+    Breakpoint(pid_t pid, std::intptr_t addr) 
+        : m_pid{pid}, m_addr{addr}, m_enabled{false}, m_saved_data{0} {}
+
+    void enable() {
+        auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+        m_saved_data = static_cast<uint8_t>(data & 0xFF);
+        uint64_t data_with_int3 = ((data & ~0xFF) | 0xCC); // inject trap
+        ptrace(PTRACE_POKEDATA, m_pid, m_addr, data_with_int3);
+        m_enabled = true;
+    }
+
+    void disable() {
+        auto data = ptrace(PTRACE_PEEKDATA, m_pid, m_addr, nullptr);
+        uint64_t restored_data = ((data & ~0xFF) | m_saved_data);
+        ptrace(PTRACE_POKEDATA, m_pid, m_addr, restored_data);
+        m_enabled = false;
+    }
+
+    bool is_enabled() const { return m_enabled; }
+
+private:
+    pid_t m_pid;
+    std::intptr_t m_addr;
+    bool m_enabled;
+    uint8_t m_saved_data;
+};
 
 std::vector<std::string> split_input(const std::string& s) {
     std::vector<std::string> out;
